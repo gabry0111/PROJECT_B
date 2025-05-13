a1f92bbc0372f91635040fac59f037ed264a7d84
DiegoArcari <diego.arcari@studio.unibo.it>
2025-05-13 15:26:20 +0200

    Started objecs implementation

---
+++ src/objects.cpp
@@ -0,0 +1,23 @@
+namespace Baba { //sarà il namespace di ogni file di questo progetto 
+    class Objects {
+        public : 
+        enum class Type {
+            Baba,
+            Wall,
+            Lava,
+            
+            max_type //maybe unused
+        };
+
+        private :
+        Type m_object_type {};
+
+        public :
+
+    }
+}
+
+int main {
+    cout << 
+    
+}
