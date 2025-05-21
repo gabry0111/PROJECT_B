7e6d31d22041ff1a026c7b1fb964541b29fdf0fe
diegoarcari <diego.arcari@studio.unibo.it>
2025-05-21 11:37:06 +0200

    aggiunto map.hpp e .cpp

---
+++ src/map.hpp
@@ -0,0 +1,37 @@
+#ifndef MAP.HPP
+#define MAP.HPP
+
+#include "enum_objects.hpp"
+//#include "objects.hpp"
+#include <array>
+#include <algorithm>
+#include <SFML/Graphics.hpp>
+
+/* map.cpp gestisce e conosce tutte le posizioni degli oggetti in ogni livello
+- getWidth / Height
+- addObjcet / remove
+- getPosition //per sapere posizione di ogni oggetto e Player
+- at() //per sapere la nuova posizione di ogni oggetto
+- load() //in futuro se vogliamo fare lettura da file
+*/
+
+namespace Baba_Is_Us{
+using position = std::pair<int,int>;
+using grid = std::array<std::array<int, 16>, 16>;
+class Map: sf::Drawable,  sf::Transformable{
+    private : 
+    grid m_level {};
+    std::size_t m_width {};
+    std::size_t m_length{};
+
+    public : 
+    Map() = default;
+    Map(grid& level) : m_level{level} {};
+    Map getMap(const std::array<int, 16*16>&) ;
+
+}
+;
+
+}
+
+#endif
